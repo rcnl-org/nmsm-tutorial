5a500308854cfa4032a552170b01677ec5e74af5
stwilliams333 <stwilliamsmail@gmail.com>
2023-07-12 16:39:10 -0500

    MTP, NCP data and scripts

---
+++ Step-3-Muscle-Tendon-Personalization/runMTP.m
@@ -0,0 +1,5 @@
+%% Run MTP and plot results
+
+close all
+
+MuscleTendonPersonalizationTool("MTPSettings.xml")
---
+++ Step-4-Neural-Control-Personalization/runNCP.m
@@ -0,0 +1,13 @@
+%% Run NCP
+
+NeuralControlPersonalizationTool("NCPSettings.xml")
+
+%% Analyze results
+
+close all
+
+plotNeuralControlPersonalizationActivations( ...
+    fullfile("ncpResults", "synergyWeights.sto"), ...
+    fullfile("ncpResults", "gait_1_synergyCommands.sto"), ...
+    fullfile("mtpResults", "muscleActivations", "gait_1_muscleActivations.sto"), ...
+    3, 3)
