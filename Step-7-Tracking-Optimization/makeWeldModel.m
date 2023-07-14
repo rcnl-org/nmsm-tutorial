25621683b6762d3ffe955c5634c9d851d6a849c2
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-07-13 19:09:08 -0500

    initial toy leg example

---
+++ Step-7-Tracking-Optimization/makeWeldModel.m
@@ -0,0 +1,17 @@
+
+initialModel = Model("RightLegAndPelvisKnee.osim");
+
+newModel = org.opensim.modeling.Model();
+
+newModel.addBody(initialModel.getBodySet().get("tibia_r"));
+newModel.addBody(initialModel.getBodySet().get("pretalus_r"));
+newModel.addBody(initialModel.getBodySet().get("talus_r"));
+newModel.addBody(initialModel.getBodySet().get("calcn_r"));
+weldJoint = org.opensim.modeling.WeldJoint("TibiaGround", newModel.getGround(), newModel.getBodySet().get(0));
+newModel.addJoint(weldJoint);
+newModel.addJoint(initialModel.getJointSet().get("ankle_r"));
+newModel.addJoint(initialModel.getJointSet().get("pretalar_r"));
+newModel.addJoint(initialModel.getJointSet().get("subtalar_r"));
+
+newModel.finalizeConnections();
+newModel.print("test.osim");
