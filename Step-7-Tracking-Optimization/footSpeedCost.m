b80c87e69e0bb9afe4b924c007865d154bfb5ef2
stwilliams333 <stwilliamsmail@gmail.com>
2023-08-06 23:28:08 -0500

    Finish foot speed cost

--- Step-7-Tracking-Optimization/footSpeedCost.m
+++ Step-7-Tracking-Optimization/footSpeedCost.m
@@ -2,10 +2,11 @@ function cost = footSpeedCost(values, modeledValues, auxdata, costTerm)
 model = auxdata.model;
 state = model.initSystem();
 for i = 1:size(state, 2)
-    model.getCoordinateSet().get(auxdata.coordinateNames{i}).setValue(state, values.statePositions(end, i))
-    model.getCoordinateSet().get(auxdata.coordinateNames{i}).setSpeedValue(state, values.stateVelocities(end, i))
+    model.getCoordinateSet().get(auxdata.coordinateNames{i}).setValue(state, values.statePositions(end, i), false);
+    model.getCoordinateSet().get(auxdata.coordinateNames{i}).setSpeedValue(state, values.stateVelocities(end, i));
 end
 model.realizeVelocity(state);
-newSpeed 
+newSpeed = model.getMarkerSet().get(costTerm.marker_name) ...
+    .getVelocityInGround(state).get(0);
 cost = costTerm.initial_speed / newSpeed;
 end
