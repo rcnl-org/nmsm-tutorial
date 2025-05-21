11dea8d79b5082baeea8f87847fcfc43f8f98160
RobSalati <rs200@rice.edu>
2025-05-21 12:24:00 -0500

    JMP tutorial

--- Step-1-Joint-Model-Personalization/RunJMPTutorial.m
+++ Step-1-Joint-Model-Personalization/RunJMPTutorial.m
@@ -1,3 +1,6 @@
+SettingsFileName = "JMPSettingsV1.xml";
+ModelFileName = "UF_Subject_3_scaled_JMPV1.osim";
+
 tic
-JointModelPersonalizationTool("JMPSettingsV2.xml")
-toc
+JointModelPersonalizationTool(SettingsFileName);
+fprintf("JMP Runtime: %f Minutes\n", toc/60);
