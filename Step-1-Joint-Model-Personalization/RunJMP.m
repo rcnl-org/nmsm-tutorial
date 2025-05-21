3f4e43367b60e1966227016ee179081325e9a5c3
Robert Salati <142843461+RobSalati@users.noreply.github.com>
2025-05-21 13:41:12 -0500

    Finish JMP tutorial setup

--- Step-1-Joint-Model-Personalization/RunJMP.m
+++ Step-1-Joint-Model-Personalization/RunJMP.m
@@ -1,11 +1,11 @@
-%% Run JMP
-JointModelPersonalizationTool("JMPBothHipSettings.xml")
+%% Run JMP V1
 
-JointModelPersonalizationTool("JMPRKnee.xml");
+SettingsFileName = "JMPSettingsV1.xml";
+outputModelFileName = "UF_Subject_3_scaled_JMPV1.osim";
 
-JointModelPersonalizationTool("JMPFemurMarkers.xml")
-
-%% Analyze Results
+tic
+JointModelPersonalizationTool(SettingsFileName);
+fprintf("JMP Runtime: %f Minutes\n", toc/60);
 
 close all
 
@@ -13,33 +13,83 @@ close all
 % knee joint.
 markerNames = ["R_Thigh_Superior", "R_Thigh_Inferior", ...
     "R_Thigh_Lateral", "R_Shank_Superior", "R_Shank_Inferior", ...
-    "R_Shank_Lateral"];
+    "R_Shank_Lateral", "L_Thigh_Superior", "L_Thigh_Inferior", ...
+    "L_Thigh_Lateral", "L_Shank_Superior", "L_Shank_Inferior", ...
+    "L_Shank_Lateral"];
 
 % Generate the .sto file for the marker error values for the original
 % model.
-reportDistanceErrorByMarker('UF_Subject_3_v2.osim', ...
-    'GaitTrial_markers_jmp.trc', markerNames, 'start.sto')
+reportDistanceErrorByMarker('UF_Subject_3_Scaled.osim', ...
+    'GaitTrial_markers_jmp.trc', markerNames, ...
+    fullfile("MarkerErrors", "ScaledModelMarkerErrors.sto"))
 
 % Generate the .sto file for the marker error values after JMP. In this
 % case, the knee isolated trial was used to optimize the knee parameters.
-reportDistanceErrorByMarker(fullfile('femurMarkerMotion', ...
-    'UF_Subject_3_hipScaling_Knee_LegMarkers.osim'), ...
-    'GaitTrial_markers_jmp.trc', markerNames, 'finish.sto')
+reportDistanceErrorByMarker(outputModelFileName, ...
+    'GaitTrial_markers_jmp.trc', markerNames, ...
+    fullfile("MarkerErrors", "JMPV1MarkerErrors.sto"))
 
 % Create the plot. A value of false means each pair is plotted separately,
 % a value of true means all are plotted together.
-plotMarkerDistanceErrors(["start.sto", "finish.sto"], false)
+plotMarkerDistanceErrors([fullfile("MarkerErrors", "ScaledModelMarkerErrors.sto"), ...
+    fullfile("MarkerErrors", "JMPV1MarkerErrors.sto")], false)
 
-%% Calculate percent improvement for markers around the knee
+%% Run JMP V2
 
-jointArray = {"knee_r"};
-markerFile = "GaitTrial_markers_jmp.trc";
+SettingsFileName = "JMPSettingsV2.xml";
+outputModelFileName = "UF_Subject_3_scaled_JMPV2.osim";
 
-start = sqrt(calculateJointError('UF_Subject_3_v2.osim', ...
-    1, jointArray, markerFile, 1e-6));
+tic
+JointModelPersonalizationTool(SettingsFileName);
+fprintf("JMP Runtime: %f Minutes\n", toc/60);
 
-final = sqrt(calculateJointError(fullfile('femurMarkerMotion', ...
-    'UF_Subject_3_hipScaling_Knee_LegMarkers.osim'), ...
-    1, jointArray, markerFile, 1e-6));
+close all
 
-disp("Marker improvement: " + (((start - final) / start) * 100) + "%") 
+% List markers of interest. for this example, it is the markers about the
+% knee joint.
+markerNames = ["R_Thigh_Superior", "R_Thigh_Inferior", ...
+    "R_Thigh_Lateral", "R_Shank_Superior", "R_Shank_Inferior", ...
+    "R_Shank_Lateral", "L_Thigh_Superior", "L_Thigh_Inferior", ...
+    "L_Thigh_Lateral", "L_Shank_Superior", "L_Shank_Inferior", ...
+    "L_Shank_Lateral"];
+
+% Generate the .sto file for the marker error values after JMP. In this
+% case, the knee isolated trial was used to optimize the knee parameters.
+reportDistanceErrorByMarker(outputModelFileName, ...
+    'GaitTrial_markers_jmp.trc', markerNames, ...
+    fullfile("MarkerErrors", "JMPV2MarkerErrors.sto"))
+
+% Create the plot. A value of false means each pair is plotted separately,
+% a value of true means all are plotted together.
+plotMarkerDistanceErrors([fullfile("MarkerErrors", "JMPV1MarkerErrors.sto"), ...
+    fullfile("MarkerErrors", "JMPV2MarkerErrors.sto")], false)
+
+%% Run JMP V3
+
+SettingsFileName = "JMPSettingsV3.xml";
+outputModelFileName = "UF_Subject_3_scaled_JMPV3.osim";
+
+tic
+JointModelPersonalizationTool(SettingsFileName);
+fprintf("JMP Runtime: %f Minutes\n", toc/60);
+
+close all
+
+% List markers of interest. for this example, it is the markers about the
+% knee joint.
+markerNames = ["R_Thigh_Superior", "R_Thigh_Inferior", ...
+    "R_Thigh_Lateral", "R_Shank_Superior", "R_Shank_Inferior", ...
+    "R_Shank_Lateral", "L_Thigh_Superior", "L_Thigh_Inferior", ...
+    "L_Thigh_Lateral", "L_Shank_Superior", "L_Shank_Inferior", ...
+    "L_Shank_Lateral"];
+
+% Generate the .sto file for the marker error values after JMP. In this
+% case, the knee isolated trial was used to optimize the knee parameters.
+reportDistanceErrorByMarker(outputModelFileName, ...
+    'GaitTrial_markers_jmp.trc', markerNames, ...
+    fullfile("MarkerErrors", "JMPV3MarkerErrors.sto"))
+
+% Create the plot. A value of false means each pair is plotted separately,
+% a value of true means all are plotted together.
+plotMarkerDistanceErrors([fullfile("MarkerErrors", "JMPV1MarkerErrors.sto"), ...
+    fullfile("MarkerErrors", "JMPV3MarkerErrors.sto")], false)
--- Step-1-Joint-Model-Personalization/RunJMPTutorial.m
+++
@@ -1,6 +0,0 @@
-SettingsFileName = "JMPSettingsV1.xml";
-ModelFileName = "UF_Subject_3_scaled_JMPV1.osim";
-
-tic
-JointModelPersonalizationTool(SettingsFileName);
-fprintf("JMP Runtime: %f Minutes\n", toc/60);
