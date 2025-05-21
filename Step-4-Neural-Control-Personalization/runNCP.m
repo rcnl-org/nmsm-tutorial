243ebef4cccf12f307e04fc1524581cf1bce1b24
Robert Salati <142843461+RobSalati@users.noreply.github.com>
2025-05-21 14:16:53 -0500

    Finish setting up NCP tutorial

--- Step-4-Neural-Control-Personalization/runNCP.m
+++ Step-4-Neural-Control-Personalization/runNCP.m
@@ -1,7 +1,16 @@
-%% Run NCP
-% close all
-% NeuralControlPersonalizationTool("NCPSettingsV1.xml")
-
-%% Analyze results
+%% Run NCP V1
+NeuralControlPersonalizationTool("NCPSettingsV1.xml")
 
 plotNcpResultsFromSettingsFile("NCPSettingsV1.xml")
+
+%% Run NCP V2
+
+NeuralControlPersonalizationTool("NCPSettingsV2.xml")
+
+plotNcpResultsFromSettingsFile("NCPSettingsV2.xml")
+
+%% Run NCP V3
+
+NeuralControlPersonalizationTool("NCPSettingsV3.xml")
+
+plotNcpResultsFromSettingsFile("NCPSettingsV3.xml")
