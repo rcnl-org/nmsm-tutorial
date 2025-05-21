27395a60be2d3584e0793a26f267835c733a75e9
Robert Salati <142843461+RobSalati@users.noreply.github.com>
2025-05-21 17:31:41 -0500

    Finish up GCP tutorial

--- Step-5-Ground-Contact-Personalization/runGCP.m
+++ Step-5-Ground-Contact-Personalization/runGCP.m
@@ -1,8 +1,17 @@
-%% Run GCP
+%% Run GCP V1
 
-GroundContactPersonalizationTool("GCPSettingsV12.xml")
-
-%% Analyze results
+GroundContactPersonalizationTool("GCPSettingsV1.xml")
 
 plotGcpResultsFromSettingsFile("GCPSettingsV1.xml")
-plotGcpResultsFromSettingsFile("GCPSettingsV12.xml")
+
+%% Run GCP V2
+
+GroundContactPersonalizationTool("GCPSettingsV2.xml")
+
+plotGcpResultsFromSettingsFile("GCPSettingsV2.xml")
+
+%% Run GCP V3
+
+GroundContactPersonalizationTool("GCPSettingsV3.xml")
+
+plotGcpResultsFromSettingsFile("GCPSettingsV3.xml")
