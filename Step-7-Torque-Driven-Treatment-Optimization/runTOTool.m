10213c7c5b34523cff2df488cbde72d0f63a49ea
RobSalati <rs200@rice.edu>
2025-10-02 17:26:02 -0500

    Treatment Optimization Tutorial 1.5.0

--- Step-7-Torque-Driven-Treatment-Optimization/runTOTool.m
+++ Step-7-Torque-Driven-Treatment-Optimization/runTOTool.m
@@ -1,9 +1,7 @@
 %% Run Torque TO V1
-
 TrackingOptimizationTool("TorqueTOSettingsV1.xml");
 
 plotTreatmentOptimizationResultsFromSettingsFile("TorqueTOSettingsV1.xml")
-
 
 %% Run Torque TO V2
 TrackingOptimizationTool("TorqueTOSettingsV2.xml");
